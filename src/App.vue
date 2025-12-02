083e71965f41cd100486ed8ca55f4a7adbb79529
Lenuary <lenyeuforia@gmail.com>
2025-12-02 22:36:54 +0100

    Configure Tailwind and updating Card component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
+
+</script>
+
 <template>
-  <h1>
-    Vue Jobs
-  </h1>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListings :limit ="3" :showButton="true" />
 </template>
