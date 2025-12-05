83c475c26b8137bb2bc84e85fdfade0ab0f32e91
Lenuary <lenyeuforia@gmail.com>
2025-12-06 00:17:45 +0100

    Visual Update

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import 'vue-toastification/dist/index.css';
 import router from './router'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
 
 const app = createApp(App)
 
+const pinia = createPinia();
+
 app.use(router);
 app.use(Toast);
+app.use(pinia);
 
 app.mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -7,44 +7,44 @@ import AddJobView from "@/views/AddJobView.vue";
 import EditJobView from "@/views/EditJobView.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: HomeView,
-        },
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
 
-        {
-            path: '/jobs',
-            name: 'jobs',
-            component: JobsView,
-        },
-        
-        {
-            path: '/jobs/:id',
-            name: 'job',
-            component: JobView,
-        },
+    {
+      path: "/jobs",
+      name: "jobs",
+      component: JobsView,
+    },
 
-        {
-            path: '/jobs/add',
-            name: 'add-job',
-            component: AddJobView,
-        },
+    {
+      path: "/jobs/:id",
+      name: "job",
+      component: JobView,
+    },
 
-        {
-            path: '/jobs/edit/:id',
-            name: 'edit-job',
-            component: EditJobView,
-        },
+    {
+      path: "/jobs/add",
+      name: "add-job",
+      component: AddJobView,
+    },
 
-        {
-            path: '/:catchAll(.*)',
-            name:'not-found',
-            component: NotFoundView,
-        }
-    ],
+    {
+      path: "/jobs/edit/:id",
+      name: "edit-job",
+      component: EditJobView,
+    },
+
+    {
+      path: "/:catchAll(.*)",
+      name: "not-found",
+      component: NotFoundView,
+    },
+  ],
 });
 
 export default router;
